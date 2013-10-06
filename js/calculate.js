76571fad0c8f21a214cdaec257b9b051b619a48f
Philip Davis <Phillysoul11@gmail.com>
2013-10-06 19:25:41 -0400

    added validation support

--- js/calculate.js
+++ js/calculate.js
@@ -1,29 +1,37 @@
 /* Function for calculating cost per person*/
 
-var distance
-var mpg
-var cpg
-var people
-var total
+var distance, mpg, cpg, people, total, rounded;
 
-//Grab the form inputs
+//Grab the form inputs and remove commas
 function setValues(){
-	distance = Number(document.getElementById("distance").value);
-	mpg = Number(document.getElementById("mpg").value);
-	cpg = Number(document.getElementById("cpg").value);
-	people = Number(document.getElementById("people").value);
+	distance = (document.getElementById("distance").value).replace(/\,/g,'');
+	mpg = (document.getElementById("mpg").value).replace(/\,/g,'');
+	cpg = (document.getElementById("cpg").value).replace(/\,/g,'');
+	people = (document.getElementById("people").value).replace(/\,/g,'');
 }
+
+
 
 // Calculate total value
 function calc() {
-	setValues()
+	setValues(); // Import variables
 	total = (distance * cpg) / (mpg * people)
-	var rounded = Math.round(total * 100) / 100
+	rounded = Math.round(total * 100) / 100
 	document.getElementById("total").value = rounded
+	validate();
 }
 
 
-// Validate that these are functions
-function isNumber(n) {
-	return !isNaN(parseFloat(n)) && isFinite(n);
+// Validate inputs
+function validate() {
+	//Check for blank values
+	javascript:$('#calculator').parsley('validate');
+	
+	//Remove any commas to allow for easy calculation
+	parseFloat('100,000.00'.replace(/,/g, ''))
+	
+	//Don't show NaN in the total field
+	if (isNaN(rounded)) {
+		document.getElementById("total").value = "";
+	}
 }
